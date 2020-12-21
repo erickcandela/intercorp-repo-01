721ea00d891e92272af0ee04c38a205a9a558a2b
Oscar Candela <erick.candela@gmail.com>
2020-12-21 12:30:17 -0500

    commit 2020.12.21 12.28.12

---
+++ project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/IndexController.java
@@ -0,0 +1,24 @@
+package pe.com.intercorp.ti.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String ejecutarIndex() {
+		String texto01 = "0";
+		String texto02 = "0";
+		String texto03 = "0";
+		String texto = "0";
+		
+		texto01 = "Greetings from Spring Boot!" + "<br/>";
+		texto02 = "Greetings from Microservicio!" + "<br/>";
+		texto03 = "[Intercorp - Experis]";
+		texto03 = "[Desarrollador: Oscar Erick Candela Carbajal (OECC)]";
+		texto = texto01 + texto02 + texto03;
+		
+		return texto;
+	}
+}
