76b33be32b11d6f7ea0f94d18a07ec49966ff95d
Oscar Candela <erick.candela@gmail.com>
2020-12-21 18:47:32 -0500

    commit 2020.12.21

--- project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerIndex.java
+++ project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerIndex.java
@@ -22,13 +22,14 @@ public class ControllerIndex {
 		String texto0101 = "0";
 		String texto0102 = "0";
 		String texto0103 = "0";
+		String texto0104 = "0";
 		String texto0100 = "0";
 		
 		texto0101 = "Greetings from Spring Boot!" + "<br/>";
 		texto0102 = "Greetings from Microservicio!" + "<br/>";
 		texto0103 = "[Intercorp - Experis]";
-		texto0103 = "[Desarrollador: Oscar Erick Candela Carbajal (OECC)]";
-		texto0100 = texto0101 + texto0102 + texto0103;
+		texto0104 = "[Desarrollador: Oscar Erick Candela Carbajal (OECC)]";
+		texto0100 = texto0101 + texto0102 + texto0103 + texto0104;
 		
 		/*==================================*/
 		
