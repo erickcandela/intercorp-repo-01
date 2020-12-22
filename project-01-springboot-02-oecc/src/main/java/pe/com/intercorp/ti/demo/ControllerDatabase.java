c50c2f1f7b1281767b8e61c0f3143b5796c8ddd1
Oscar Candela <erick.candela@gmail.com>
2020-12-21 22:39:26 -0500

    commit

--- project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerDatabase.java
+++ project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerDatabase.java
@@ -28,18 +28,31 @@ public class ControllerDatabase {
 		String texto0204 = "0";
 		String texto0200 = "0";
 
-		/*
+		String propertyUrl = "0";
+		String propertyUser = "0";
+		String propertyPassword = "0";
+		
 		texto0201 = "Loading application properties" + "<br/>";
 		Properties properties = new Properties();
 		properties.load(ControllerDatabase.class.getClassLoader().getResourceAsStream("application.properties"));
 		
+		propertyUrl = properties.getProperty("url");
+		System.out.println(propertyUrl);
+		
+		propertyUser = properties.getProperty("user");
+		System.out.println(propertyUser);
+		
+		propertyPassword = properties.getProperty("password");
+		System.out.println(propertyPassword);
+		
 		texto0202 = "Connecting to the database" + "<br/>";
-		Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+		//Connection connection = DriverManager.getConnection(propertyUrl, properties);
+		Connection connection = DriverManager.getConnection(propertyUrl, propertyUser, propertyPassword);
 		texto0203 = "Database connection test: " + connection.getCatalog() + "<br/>";
 		
 		texto0204 = "Closing database connection" + "<br/>";
 		connection.close();		
-		*/
+		
 		texto0200 = texto0201 + texto0202 + texto0203 + texto0204;	
 		System.out.println("ejecutarDatabase FIN");
 		
