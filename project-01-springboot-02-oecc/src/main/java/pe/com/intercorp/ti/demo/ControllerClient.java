963896b6fbf6236d79f7d778726d6db0b80b1245
Oscar Candela <erick.candela@gmail.com>
2020-12-22 01:50:23 -0500

    commit

--- project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerClient.java
+++ project-01-springboot-02-oecc/src/main/java/pe/com/intercorp/ti/demo/ControllerClient.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +24,101 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @ComponentScan(basePackages={"pe.com.intercorp.ti.demo"})
 public class ControllerClient {
 
+	@RequestMapping("/listClient")
+	public String listClient() {
+		System.out.println("listClient INICIO");
+		String texto0201 = "0";
+		String texto0202 = "0";
+		String texto0203 = "0";
+		String texto0204 = "0";
+		String texto0200 = "0";
+		
+		String error01 = "0";
+		String error02 = "0";
+		String error03 = "0";
+		
+		String propertyUrl = "0";
+		String propertyUser = "0";
+		String propertyPassword = "0";
+		
+		String listContent = "0";
+		
+		try {
+			
+			texto0201 = "Loading application properties - List" + "<br/>";
+			Properties properties = new Properties();
+			properties.load(ControllerClient.class.getClassLoader().getResourceAsStream("application.properties"));
+			
+			propertyUrl = properties.getProperty("url");
+			System.out.println(propertyUrl);
+			
+			propertyUser = properties.getProperty("user");
+			System.out.println(propertyUser);
+			
+			propertyPassword = properties.getProperty("password");
+			System.out.println(propertyPassword);
+			
+			texto0202 = "Connecting to the database - List" + "<br/>";
+			
+			Connection connection = DriverManager.getConnection(propertyUrl, propertyUser, propertyPassword);
+			texto0203 = "Database connection test - List: " + connection.getCatalog() + "<br/>";
+	
+			Long id = 0L;
+			String nombre = "0";
+			String apellidoPaterno = "0";
+			String apellidoMaterno = "0";
+			String edad = "0";
+			String fechaNacimiento = "0";
+			
+			PreparedStatement readStatement = connection.prepareStatement("SELECT * FROM client;");
+		    ResultSet resultSet = readStatement.executeQuery();
+		    ResultSetMetaData md = resultSet.getMetaData();
+		    
+		    List<BClient> objetList = new ArrayList<BClient>();
+		    
+		    if (!resultSet.next()) {
+		    	error03 = "There is no data in the database!";
+		    	texto0200 += error03;
+		        return null;
+		    }
+		    
+			while (resultSet.next()) {
+			 
+				id = resultSet.getLong("id");
+				nombre = resultSet.getString("username");
+				apellidoPaterno = resultSet.getString("apellidopaterno");
+				apellidoMaterno = resultSet.getString("apellidomaterno");
+				edad = resultSet.getString("edad");
+				fechaNacimiento = resultSet.getString("fechanacimiento");
+			  
+				BClient objetBean = new BClient();
+				objetBean.setId(id);
+			
+			  objetList.add(objetBean);
+			}
+		    
+		    for (int i = 0; i<objetList.size(); i++) {
+		    	listContent = objetList.get(i).getId() + "; " + objetList.get(i).getApellidoPaterno() + "; " + objetList.get(i).getApellidoMaterno() + "; " + objetList.get(i).getEdad() + "; " + objetList.get(i).getFechaNacimiento() + "<br/>";  
+		    }
+		     
+			texto0204 = "Closing database connection - List" + "<br/>";
+			connection.close();	
+			
+			
+		} catch (IOException ioe) {
+			error01 = ioe.getMessage() + "<br/>";
+			texto0200 += error01;
+		} catch (SQLException sqle) {
+			error02 = sqle.getMessage() + "<br/>";
+			texto0200 += error02;
+		}
+		
+		texto0200 += texto0201 + texto0202 + texto0203 + texto0204 + listContent;	
+		System.out.println("listClient FIN");
+				
+		return texto0200;
+	}	
+	
 	@RequestMapping("/insertClient")
 	public String insertClient() {
 		System.out.println("insertClient INICIO");
@@ -56,7 +155,7 @@ public class ControllerClient {
 			Connection connection = DriverManager.getConnection(propertyUrl, propertyUser, propertyPassword);
 			texto0203 = "Database connection test - Insert: " + connection.getCatalog() + "<br/>";
 	
-			Long id = parseLong(0);
+			Long id = 0L;
 			String nombre = "0";
 			String apellidoPaterno = "0";
 			String apellidoMaterno = "0";
@@ -65,7 +164,7 @@ public class ControllerClient {
 			
 			PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO client (id, nombre, apellidopaterno, apellidomaterno, edad, fechanacimiento) VALUES (?, ?, ?, ?, ?, ?);");
 			
-			id = parseLong(3);
+			id = 3L;
 			nombre = "Franklin";
 			apellidoPaterno = "Gómez";
 			apellidoMaterno = "Condori";
@@ -106,8 +205,4 @@ public class ControllerClient {
 		return texto0200;
 	}
 
-	private Long parseLong(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
